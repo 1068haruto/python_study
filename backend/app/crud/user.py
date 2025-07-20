8c6f87395c8365054155bd6d6fb86479bcc362be
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-07-20 19:47:53 +0900

    feat: define DB operations for user create & delete

---
+++ backend/app/crud/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(name=user.name, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return True
+    return False
