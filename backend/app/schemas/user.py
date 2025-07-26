696e3ae4b008ecdd96e7bff97e4305b80a0a74bb
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-07-26 23:29:24 +0900

    feat: add endpoint & schema for users list

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel
 class UserCreate(BaseModel):
     name: str
     password: str
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
