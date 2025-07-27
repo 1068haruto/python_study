84985546f81ee9c3af3ab3d5feb6a9fa715a1213
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-07-27 18:39:40 +0900

    feat: add method, router, schema for update func

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserUpdate
 
 def create_user(db: Session, user: UserCreate):
     db_user = User(name=user.name, password=user.password)
@@ -9,10 +9,22 @@ def create_user(db: Session, user: UserCreate):
     db.refresh(db_user)
     return db_user
 
+def update_user(db: Session, user_id: int, user_data: UserUpdate):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        # 入力値のみ更新
+        if user_data.name is not None and user_data.name != "":
+            db_user.name = user_data.name
+        if user_data.password is not None and user_data.password != "":
+            db_user.password = user_data.password
+        db.commit()
+        return db_user
+    return None
+
 def delete_user(db: Session, user_id: int):
     user = db.query(User).filter(User.id == user_id).first()
     if user:
         db.delete(user)
         db.commit()
-        return True  # ← 成功時
-    return False     # ← 失敗時
+        return True
+    return False
--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,22 +1,33 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.schemas.user import UserCreate, UserOut
+from app.schemas.user import UserCreate, UserOut, UserUpdate
 from app.models.user import User
-from app.crud.user import create_user, delete_user
+from app.crud.user import create_user, update_user, delete_user
 
 router = APIRouter()
 
+# 作成実行時
 @router.post("/users/create")
 def create_user_endpoint(user: UserCreate, db: Session = Depends(get_db)):
     created_user = create_user(db, user)
     return {"message": "作成成功！", "user_id": created_user.id}
 
+# 一覧表示時
 @router.get("/users/list", response_model=list[UserOut])
 def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
 
+# 更新実行時
+@router.put("/users/update/{user_id}")
+def update_user_endpoint(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    updated_user = update_user(db, user_id, user)
+    if updated_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"message": "更新成功！", "user_id": updated_user.id}
+
+# 削除実行時
 @router.delete("/users/delete/{user_id}")
 def delete_user_endpoint(user_id: int, db: Session = Depends(get_db)):
     success = delete_user(db, user_id)
