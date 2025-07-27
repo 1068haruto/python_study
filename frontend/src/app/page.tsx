cdcc0df021bda85dd89d4514736ef6c1e70d631d
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-07-27 21:36:43 +0900

    improve: update link & style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,14 +4,10 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Home</h1>
-      <p>ユーザーの作成と削除ができる</p>
-
+      <h1 className={styles.title}>🏠ホーム</h1>
+      <p>機能を選択</p>
       <div className={styles.links}>
-        <Link href="/users/list"><button>一覧ページ</button></Link>
-        <Link href="/users/create"><button>作成ページ</button></Link>
-        <Link href="/users/update"><button>更新ページ</button></Link>
-        <Link href="/users/delete"><button>削除ページ</button></Link>
+        <Link href="/users" className={styles.linkButton}>ユーザー機能</Link>
       </div>
     </main>
   );
