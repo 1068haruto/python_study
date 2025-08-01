e871abb7dc3d493fc031a91edb79c46fb8e175c8
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-08-01 20:28:05 +0900

    feat: add redirect logic after user logout

--- frontend/src/app/users/logout/LogoutButton.tsx
+++ frontend/src/app/users/logout/LogoutButton.tsx
@@ -1,21 +1,28 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/contexts/AuthContext';
 import styles from './LogoutButton.module.scss';
 
-
 export default function LogoutButton() {
+  const router = useRouter();
+  const { setUser } = useAuth();
 	const [message, setMessage] = useState('');
 
 	const handleLogout = async () => {
 		try {
 			const res = await fetch('http://localhost:8000/logout', {
 				method: 'POST',
-				credentials: 'include',  // ← Cookie 送信
+				credentials: 'include',
 			})
 
-			const data = await res.json();
-			setMessage(data.message);
+			if (!res.ok) {
+        throw new Error('おっと、レスポンスが正常じゃないぞ。');
+      }
+
+			setUser(null);
+			router.push('/');
 		} catch (error) {
 			setMessage('ログアウト失敗。');
 		}	
