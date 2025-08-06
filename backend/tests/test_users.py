bc04f58c680be71b18322a40d8c3bb599e9650a0
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-08-07 05:45:07 +0900

    test: refactor using fixture

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -21,16 +21,9 @@ def test_read_users():
     assert isinstance(response.json(), list)  # レスポンスボディがリスト型であることを確認
 
 
-def test_update_user():
-    # 前処理: 更新対象のユーザー作成
-    create_response = client.post("/users/create", json={
-        "name": "user_to_be_updated",
-        "password": "testpassword"
-    })
-    assert create_response.status_code == 200
-    created_user_id = create_response.json()["user_id"]
-    
-    # 本処理
+def test_update_user(create_test_user):
+    created_user_id = create_test_user
+
     response = client.put(f"/users/update/{created_user_id}", json={
         "name": "updated_user",
         "password": "updated_password"
@@ -39,17 +32,9 @@ def test_update_user():
     assert response.json()["message"] == "更新成功！"
 
 
-# ユーザー削除（DELETE）
-def test_delete_user():
-    # 前処理: 削除対象のユーザー作成
-    create_response = client.post("/users/create", json={
-        "name": "user_to_be_deleted",
-        "password": "testpassword"
-    })
-    assert create_response.status_code == 200
-    created_user_id = create_response.json()["user_id"]
+def test_delete_user(create_test_user):
+    created_user_id = create_test_user
 
-    # 本処理
     response = client.delete(f"/users/delete/{created_user_id}")
     assert response.status_code == 200
     assert response.json()["message"] == "削除成功！"
